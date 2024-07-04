00068e95bb5b59f7f0de30934c382058e9254d48
renan <renan.nic28@gmail.com>
2024-07-04 08:15:45 -0300

    first commit

---
+++ src/main/java/me/dio/diojavaapirestfull/DioJavaApiRestfullApplication.java
@@ -0,0 +1,13 @@
+package me.dio.diojavaapirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaApiRestfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioJavaApiRestfullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dio/diojavaapirestfull/DioJavaApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.diojavaapirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaApiRestfullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
