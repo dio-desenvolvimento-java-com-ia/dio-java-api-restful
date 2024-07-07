e7b1d0aa95f79795a2c41ed5cb7518da67b06203
renan <renan.nic28@gmail.com>
2024-07-07 16:45:42 -0300

    correction CORS error

--- src/main/java/me/dio/diojavaapirestfull/DioJavaApiRestfullApplication.java
+++ src/main/java/me/dio/diojavaapirestfull/DioJavaApiRestfullApplication.java
@@ -1,8 +1,11 @@
 package me.dio.diojavaapirestfull;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//* Solução do erro de CORS
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 @SpringBootApplication
 public class DioJavaApiRestfullApplication {
 
